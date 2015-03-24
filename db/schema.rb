da38aa7eb08ed37e0662a7347dbaf9cea133c91f
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-24 08:58:18 -0500

    adds notes column to bills

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324135342) do
+ActiveRecord::Schema.define(version: 20150324135735) do
 
   create_table "bills", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150324135342) do
     t.datetime "updated_at", null: false
     t.datetime "due_date"
     t.datetime "reminder"
+    t.text     "notes"
   end
 
   create_table "users", force: :cascade do |t|
