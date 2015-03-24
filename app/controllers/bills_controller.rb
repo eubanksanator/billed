66ca0b720ffa842f37cf7a289235093c24eec35e
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-24 09:42:40 -0500

    adjusts styles to table

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -70,6 +70,6 @@ class BillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bill_params
-      params.require(:bill).permit(:name, :due_date, :user_id)
+      params.require(:bill).permit(:name, :due_date, :notes, :reminder, :user_id)
     end
 end
