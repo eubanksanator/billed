bed49f66de23bcf76146d8e633c4553eeaf89c23
Travis Eubanks <TRAVIS@traviss-air.attlocal.net>
2015-03-24 12:04:28 -0500

    adds new time display in environments

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#changes the format display of month,day,year (must call .to_s(:due_time)) to run this format.
+Time::DATE_FORMATS[:due_time] = "%b %d,  %Y"
